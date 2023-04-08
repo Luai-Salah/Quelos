1020b50969247858784d79e0e9c8564bdd87f75f
Luai-Salah <77834783+Luai-Salah@users.noreply.github.com>
2023-04-08 12:05:50 +0200

    Update OpenGLContext.cpp

--- Quelos/src/Platform/OpenGL/OpenGLContext.cpp
+++ Quelos/src/Platform/OpenGL/OpenGLContext.cpp
@@ -19,10 +19,10 @@ namespace Quelos
 		int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
 		QS_CORE_ASSERT(status, "Failed to initialize Glad!");
 
-		QS_CORE_INFO("OpenGL Info:\n\tVendor: {0}\n\tRenderer: {1}\n\tVersion: {2})",
-			glGetString(GL_VENDOR),
-			glGetString(GL_RENDERER),
-			glGetString(GL_VERSION)
+		QS_CORE_INFO("OpenG Info:\n\tVendor: {}\n\tRenderer: {}\n\tVersion: {})",
+			(const char*)glGetString(GL_VENDOR),
+			(const char*)glGetString(GL_RENDERER),
+			(const char*)glGetString(GL_VERSION)
 		);
 	}
 
