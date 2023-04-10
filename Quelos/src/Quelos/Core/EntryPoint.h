cde37da4902f1c6e1346a57350f1b4b6ff37bbea
Luai Salah <luaisalah9@gmail.com>
2023-04-10 08:33:48 +0200

    Change the profiling folder to Logs

--- Quelos/src/Quelos/Core/EntryPoint.h
+++ Quelos/src/Quelos/Core/EntryPoint.h
@@ -6,15 +6,15 @@ extern Quelos::Application* Quelos::CreateApplication(const Quelos::ApplicationC
 
 int main(int argc, char* argv[])
 {
-	QS_PROFILE_BEGIN_SESSION("Startup", "Profiling\\QS_Profile-Startup.json");
+	QS_PROFILE_BEGIN_SESSION("Startup", "Logs\\Profiling\\QS_Profile-Startup.json");
 	auto app = Quelos::CreateApplication(Quelos::ApplicationCommandLineArgs { argc, argv });
 	QS_PROFILE_END_SESSION();
 
-	QS_PROFILE_BEGIN_SESSION("Runtime", "Profiling\\QS_Profile-Runtime.json");
+	QS_PROFILE_BEGIN_SESSION("Runtime", "Logs\\Profiling\\QS_Profile-Runtime.json");
 	app->Run();
 	QS_PROFILE_END_SESSION();
 
-	QS_PROFILE_BEGIN_SESSION("Shutdown", "Profiling\\QS_Profile-Shutdown.json");
+	QS_PROFILE_BEGIN_SESSION("Shutdown", "Logs\\Profiling\\QS_Profile-Shutdown.json");
 	delete app;
 	QS_PROFILE_END_SESSION();
 
